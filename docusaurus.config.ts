a03ee672894adafb455d85c228b1aeb2eedf9f2d
johnycho <shunnn215@gmail.com>
2025-01-18 20:57:13 +0900

    자동 배포 테스트

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: '이다예',
-  tagline: '조이 언어발달센터',
+  title: '이다예123',
+  tagline: '조이 언어발달센터123',
   favicon: 'img/favicon.ico',
 
   // GitHub Pages URL 설정
