6615e71d49a94dd5017ff005d948e8e049ea8cdd
johnycho <shunnn215@gmail.com>
2025-06-24 02:34:29 +0900

    배너 영역 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,11 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container" style={{fontFamily: "'Nanum Pen Script', cursive", color: "#fff"}}>
-        <Heading as="h1" className="hero__title"
-                 style={{fontFamily: "'Nanum Pen Script', cursive", color: "#fff"}}>
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        {/*<Heading as="h1" className="hero__title"*/}
+        {/*         style={{fontFamily: "'Nanum Pen Script', cursive", color: "#fff"}}>*/}
+        {/*  {siteConfig.title}*/}
+        {/*</Heading>*/}
+        {/*<p className="hero__subtitle">{siteConfig.tagline}</p>*/}
         {/*<div className={styles.buttons}>*/}
         {/*  <Link*/}
         {/*    className="button button--secondary button--lg"*/}
