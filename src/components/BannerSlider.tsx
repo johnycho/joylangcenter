2df1dda8cd18aa09891baed7aa9b4d8f17303627
johnycho <shunnn215@gmail.com>
2025-07-15 22:35:56 +0900

    배너 이미지 추가

--- src/components/BannerSlider.tsx
+++ src/components/BannerSlider.tsx
@@ -17,7 +17,9 @@ import styles from './BannerSlider.module.css';
 export default function BannerSlider() {
   const [slides] = useState([
     '/img/index-banner.jpg',
-    '/img/index-banner.jpg',
+    '/img/index-banner-2.jpg',
+    '/img/index-banner-3.jpg',
+    '/img/index-banner-4.jpg',
   ]);
 
   return (
