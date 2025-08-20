c8e1065cdb969f307c2c1649ba1baff1bad7b2d8
johnycho <shunnn215@gmail.com>
2025-08-20 20:39:28 +0900

    :wrench: 커뮤니티 메뉴 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -151,6 +151,7 @@ const config: Config = {
           items: [
             {label: "공지사항", to: "/blog/tags/notice"},
             {label: "센터소식", to: "/blog/tags/news"},
+            {label: "교육자료", to: "/blog/tags/library"},
           ],
         },
         {
